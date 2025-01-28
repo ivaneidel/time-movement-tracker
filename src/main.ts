e8055f5b9617873f22046739b21e82acd3f3e74f
Ivan Eidelstein <ivan.eidelstein@gmail.com>
2025-01-28 17:42:15 -0300

    Add: Prevent close warning

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,21 @@
-import { app, BrowserWindow } from "electron";
+import { app, BrowserWindow, dialog } from "electron";
 import * as path from "path";
 
 let mainWindow: BrowserWindow | null = null;
+
+const killApp = async () => {
+  const result = await dialog.showMessageBox(mainWindow!, {
+    type: "warning",
+    buttons: ["Yes", "No"],
+    defaultId: 1,
+    title: "Quit app",
+    message: "You are about to close the app.\nAre you sure?",
+  });
+
+  if ((result as any).response === 0) {
+    mainWindow?.destroy();
+  }
+};
 
 const createWindow = () => {
   mainWindow = new BrowserWindow({
@@ -14,13 +28,19 @@ const createWindow = () => {
   });
 
   mainWindow.loadFile(path.join(__dirname, "index.html"));
+
+  mainWindow.on("close", (event) => {
+    event.preventDefault();
+    killApp();
+  });
 };
 
 app.on("ready", createWindow);
 
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
+app.on("before-quit", (event) => {
+  if (mainWindow && !mainWindow.isDestroyed) {
+    event.preventDefault();
+    killApp();
   }
 });
 
